0eb8d43bea580394ac686cecc010a9b57b5b4175
Danilo Cucharro <danilocucharro@Danilos-MacBook-Air.local>
2026-01-31 13:47:56 -0300

    feat: applying a header component

---
+++ src/app/(board)/page.tsx
@@ -0,0 +1,43 @@
+import { Button } from "@/components/button";
+import { Card } from "@/components/card";
+import { Section } from "@/components/section";
+import { ArchiveIcon, MessageCircleIcon, ThumbsUpIcon } from "lucide-react";
+
+export default function Home() {
+  return (
+    <main className="grid grid-cols-4 gap-5 flex-1 items-stretch">
+      <Section.Root>
+        {/* Header */}
+        <Section.Header>
+          <Section.Title>
+            <ArchiveIcon className="size-3" />
+            Backlog
+          </Section.Title>
+
+          <Section.IssueCount>32</Section.IssueCount>
+        </Section.Header>
+
+        {/* Content */}
+        <Section.Content>
+          <Card.Root>
+            <Card.Header>
+              <Card.Number>ECO-001</Card.Number>
+              <Card.Title>Implementar cartao de credito</Card.Title>
+            </Card.Header>
+            <Card.Footer>
+              <Button>
+                <ThumbsUpIcon className="size-3" />
+                <span className="text-sm">12</span>
+              </Button>
+
+              <Button>
+                <MessageCircleIcon className="size-3" />
+                <span className="text-sm">4</span>
+              </Button>
+            </Card.Footer>
+          </Card.Root>
+        </Section.Content>
+      </Section.Root>
+    </main>
+  );
+}
