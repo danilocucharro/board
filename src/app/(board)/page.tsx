e7de1dc5062fea7880a908de84f262ad586af4e3
Danilo Cucharro <danilocucharro@Danilos-MacBook-Air.local>
2026-02-02 16:49:55 -0300

    feat: Applying the fetch in the issues storaged

--- src/app/(board)/page.tsx
+++ src/app/(board)/page.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/button";
 import { Card } from "@/components/card";
 import { Section } from "@/components/section";
+import { listIssues } from "@/http/list-issues";
 import { ArchiveIcon, MessageCircleIcon, ThumbsUpIcon } from "lucide-react";
 import { Metadata } from "next";
 
@@ -14,7 +15,9 @@ interface BoardProps {
 
 export default async function Board({ searchParams }: BoardProps) {
   const { q } = await searchParams;
-  console.log(q);
+
+  const issues = await listIssues();
+
   return (
     <main className="grid grid-cols-4 gap-5 flex-1 items-stretch">
       <Section.Root>
@@ -25,28 +28,143 @@ export default async function Board({ searchParams }: BoardProps) {
             Backlog
           </Section.Title>
 
-          <Section.IssueCount>32</Section.IssueCount>
+          <Section.IssueCount>{issues.backlog.length}</Section.IssueCount>
         </Section.Header>
 
         {/* Content */}
         <Section.Content>
-          <Card.Root>
-            <Card.Header>
-              <Card.Number>ECO-001</Card.Number>
-              <Card.Title>Implementar cartao de credito</Card.Title>
-            </Card.Header>
-            <Card.Footer>
-              <Button>
-                <ThumbsUpIcon className="size-3" />
-                <span className="text-sm">12</span>
-              </Button>
+          {issues.backlog.map((issue) => {
+            return (
+              <Card.Root key={issue.id}>
+                <Card.Header>
+                  <Card.Number>ISS-{issue.issueNumber}</Card.Number>
+                  <Card.Title>{issue.title}</Card.Title>
+                </Card.Header>
+                <Card.Footer>
+                  <Button>
+                    <ThumbsUpIcon className="size-3" />
+                    <span className="text-sm">12</span>
+                  </Button>
 
-              <Button>
-                <MessageCircleIcon className="size-3" />
-                <span className="text-sm">4</span>
-              </Button>
-            </Card.Footer>
-          </Card.Root>
+                  <Button>
+                    <MessageCircleIcon className="size-3" />
+                    <span className="text-sm">4</span>
+                  </Button>
+                </Card.Footer>
+              </Card.Root>
+            );
+          })}
+        </Section.Content>
+      </Section.Root>
+
+      <Section.Root>
+        {/* Header */}
+        <Section.Header>
+          <Section.Title>
+            <ArchiveIcon className="size-3" />
+            To-do
+          </Section.Title>
+
+          <Section.IssueCount>{issues.todo.length}</Section.IssueCount>
+        </Section.Header>
+
+        {/* Content */}
+        <Section.Content>
+          {issues.todo.map((issue) => {
+            return (
+              <Card.Root key={issue.id}>
+                <Card.Header>
+                  <Card.Number>ISS-{issue.issueNumber}</Card.Number>
+                  <Card.Title>{issue.title}</Card.Title>
+                </Card.Header>
+                <Card.Footer>
+                  <Button>
+                    <ThumbsUpIcon className="size-3" />
+                    <span className="text-sm">12</span>
+                  </Button>
+
+                  <Button>
+                    <MessageCircleIcon className="size-3" />
+                    <span className="text-sm">4</span>
+                  </Button>
+                </Card.Footer>
+              </Card.Root>
+            );
+          })}
+        </Section.Content>
+      </Section.Root>
+
+      <Section.Root>
+        {/* Header */}
+        <Section.Header>
+          <Section.Title>
+            <ArchiveIcon className="size-3" />
+            In Progress
+          </Section.Title>
+
+          <Section.IssueCount>{issues.in_progress.length}</Section.IssueCount>
+        </Section.Header>
+
+        {/* Content */}
+        <Section.Content>
+          {issues.in_progress.map((issue) => {
+            return (
+              <Card.Root key={issue.id}>
+                <Card.Header>
+                  <Card.Number>ISS-{issue.issueNumber}</Card.Number>
+                  <Card.Title>{issue.title}</Card.Title>
+                </Card.Header>
+                <Card.Footer>
+                  <Button>
+                    <ThumbsUpIcon className="size-3" />
+                    <span className="text-sm">12</span>
+                  </Button>
+
+                  <Button>
+                    <MessageCircleIcon className="size-3" />
+                    <span className="text-sm">4</span>
+                  </Button>
+                </Card.Footer>
+              </Card.Root>
+            );
+          })}
+        </Section.Content>
+      </Section.Root>
+
+      <Section.Root>
+        {/* Header */}
+        <Section.Header>
+          <Section.Title>
+            <ArchiveIcon className="size-3" />
+            Done
+          </Section.Title>
+
+          <Section.IssueCount>{issues.done.length}</Section.IssueCount>
+        </Section.Header>
+
+        {/* Content */}
+        <Section.Content>
+          {issues.done.map((issue) => {
+            return (
+              <Card.Root key={issue.id}>
+                <Card.Header>
+                  <Card.Number>ISS-{issue.issueNumber}</Card.Number>
+                  <Card.Title>{issue.title}</Card.Title>
+                </Card.Header>
+                <Card.Footer>
+                  <Button>
+                    <ThumbsUpIcon className="size-3" />
+                    <span className="text-sm">12</span>
+                  </Button>
+
+                  <Button>
+                    <MessageCircleIcon className="size-3" />
+                    <span className="text-sm">4</span>
+                  </Button>
+                </Card.Footer>
+              </Card.Root>
+            );
+          })}
         </Section.Content>
       </Section.Root>
     </main>
