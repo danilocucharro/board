9ab7a8b15a20f4e37c4a358cc46969652ac2fbc5
Danilo Cucharro <danilocucharro@Danilos-MacBook-Air.local>
2026-02-12 10:29:39 -0300

    feat: Adding a modal when clicking on issue details

--- src/app/issues/layout.tsx
+++ src/app/issues/layout.tsx
@@ -8,7 +8,6 @@ export default function BoardLayout({
   return (
     <div className="max-w-405 w-full mx-auto p-10 flex flex-col gap-8 h-dvh">
       <Header />
-
       {children}
     </div>
   );
